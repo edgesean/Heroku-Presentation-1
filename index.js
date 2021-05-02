1c5c1cd0a83004bf8125365b01f8cfcbf4811e37
Sean Dillon <seandillon59@gmail.com>
2021-05-02 20:33:57 +0200

    .

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => console.log(`running on localhost:${port}`))
+app.listen(process.env.PORT || port, () => console.log('running on port localhost:' + port));
 
